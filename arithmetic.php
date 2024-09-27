d492e8c799a30ed393c81e74b19191dab6beea1e
micapacut <pacutmica09@gmail.com>
2024-09-26 20:44:34 -0800

    Update arithmetic.php

--- arithmetic.php
+++ arithmetic.php
@@ -1,17 +1,58 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Arithmetic</title>
-</head>
-<body>
-    <form action= "secondarth.php" method="post">
-        <label for="num1">Enter the first number:</label>
-        <input type="number" name="num1"><br><br>
-        <label for="num2">Enter the second number:</label>
-        <input type="number" name="num2"><br><br>
-        <input type="submit" value="Calculate">
-    </form>
-</body>
-</html>
+<?php
+  
+  $num1 = "";
+  $num2 = "";
+  $sum = "";
+  $difference = "";
+  $product = "";
+  $quotient = "";
+  $modulo = "";
+
+  
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $num1 = $_POST["num1"];
+    $num2 = $_POST["num2"];
+
+   
+    $sum = $num1 + $num2;
+    $difference = $num1 - $num2;
+    $product = $num1 * $num2;
+    if ($num2 != 0) {
+      $quotient = $num1 / $num2;
+      $modulo = $num1 % $num2;
+    } else {
+      $quotient = "Cannot divide by zero!";
+      $modulo = "Cannot divide by zero!";
+    }
+  }
+?>
+
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+
+
+<link rel="stylesheet" type="text/css" href="style.css">
+
+<div class="container">
+  <h1>Arithmetic</h1>
+  <form method="post">
+    <div class="form-group">
+      <label for="num1">First Number:</label>
+      <input type="number" class="form-control" id="num1" name="num1" value="<?php echo $num1; ?>">
+    </div>
+    <div class="form-group">
+      <label for="num2">Second Number:</label>
+      <input type="number" class="form-control" id="num2" name="num2" value="<?php echo $num2; ?>">
+    </div>
+    <button type="submit" class="btn btn-primary">Calculate</button>
+  </form>
+
+  <div class="results">
+    <h2>Results:</h2>
+    <p>Sum: <?php echo $sum; ?></p>
+    <p>Difference: <?php echo $difference; ?></p>
+    <p>Product: <?php echo $product; ?></p>
+    <p>Quotient: <?php echo $quotient; ?></p>
+    <p>Modulo: <?php echo $modulo; ?></p>
+  </div>
+</div>
